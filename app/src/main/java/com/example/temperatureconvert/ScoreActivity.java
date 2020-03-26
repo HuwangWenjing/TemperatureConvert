3e8e4a8d2ec2da0c3da4418cb5686a9f4994b885
HuwangWenjing <chloehwang@126.com>
2020-03-26 16:09:34 +0800

    计分器拓展——两个队

--- app/src/main/java/com/example/temperatureconvert/ScoreActivity.java
+++ app/src/main/java/com/example/temperatureconvert/ScoreActivity.java
@@ -10,28 +10,41 @@ import android.widget.TextView;
 public class ScoreActivity extends AppCompatActivity {
 
     TextView score;
-
+    TextView scoreB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
 
         score = (TextView) findViewById(R.id.scoreA);
-
+        scoreB = (TextView)findViewById(R.id.scoreB);
     }
 
     public void btnAdd1(View btn) {
-        showScore(1);
+        if(btn.getId()== R.id.btn_1){  //多个按钮调用同一个方法可这样区分
+            showScore(1);
+        }else{
+            showScore2(1);
+        }
     }
     public void btnAdd2(View btn) {
-        showScore(2);
+        if(btn.getId()== R.id.btn_2){  //多个按钮调用同一个方法可这样区分
+            showScore(2);
+        }else{
+            showScore2(2);
+        }
     }
     public void btnAdd3(View btn) {
-        showScore(3);
+        if(btn.getId()== R.id.btn_3){  //多个按钮调用同一个方法可这样区分
+            showScore(3);
+        }else{
+            showScore2(3);
+        }
     }
 
     public void btnReset(View btn) {
         score.setText("0");
+        scoreB.setText("0");
     }
 
     private void showScore(int inc) {
@@ -39,5 +52,12 @@ public class ScoreActivity extends AppCompatActivity {
         String oldScore = (String)score.getText();
         int newScore = Integer.parseInt(oldScore) + inc;
         score.setText("" + newScore);
+    }
+
+    private void showScore2(int inc) {
+        Log.i("show", "inc=" + inc);
+        String oldScore = (String)score.getText();
+        int newScore = Integer.parseInt(oldScore) + inc;
+        scoreB.setText("" + newScore);
     }
 }
