8816b1858645f7d80c30d4b4eb658d1994c91487
HuwangWenjing <chloehwang@126.com>
2020-04-02 16:30:25 +0800

    处理汇率配置页面、参数的传递与返回、菜单的使用

--- app/src/main/java/com/example/temperatureconvert/RateActivity.java
+++ app/src/main/java/com/example/temperatureconvert/RateActivity.java
@@ -1,17 +1,27 @@
 package com.example.temperatureconvert;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RateActivity extends AppCompatActivity {
 
     EditText rmb;
     TextView show;
+    float dollarRate = 6.7f;
+    float euroRate = 11.0f;
+    float wonRate = 500f;
+    public final String TAG = "RateActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,15 +43,70 @@ public class RateActivity extends AppCompatActivity {
 
         float val;
         if(btn.getId() == R.id.Dollar){
-            val = r * (1/6.7f);
-            show.setText(String.valueOf(val));
+            show.setText(String.format("%2f",r/dollarRate));
         }else if(btn.getId() == R.id.EURO){
-            val = r * (1/11.0f);
-            show.setText(String.valueOf(val));
+            show.setText(String.format("%2f",r/euroRate));
         }
         else{
-            val = r * 500;
-            show.setText(String.valueOf(val));
+            show.setText(String.format("%2f",r*wonRate));
         }
+    }
+
+    public void openOne(View btn) {
+        //打开一个页面
+        //Log.i("open", "openOne");
+        //打开新页面
+        //Intent hello = new Intent(this,ScoreActivity.class);
+        //startActivity(hello);
+        //打开网址
+        //Intent web = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.jd.com"));
+        //startActivity(web);
+        openAdd();
+    }
+
+    public void openAdd() {
+        Intent add = new Intent(this,AddActivity.class);
+        startActivity(add);
+        //将变量存进去
+        add.putExtra("dollarRate",dollarRate);
+        add.putExtra("euroRate",euroRate);
+        add.putExtra("wonRate",wonRate);
+        Log.i(TAG,"openOne: dollarRate= " + dollarRate);
+        Log.i(TAG,"openOne: euroRate= " + euroRate);
+        Log.i(TAG,"openOne: wonRate= " + wonRate);
+
+        startActivityForResult(add,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == 2){
+            /*bdl.putFloat("key_newDollar",newDollar);
+            bdl.putFloat("key_newEuro",newEuro);
+            bdl.putFloat("key_newWon",newWon);*/
+            Bundle bundle = data.getExtras();
+            dollarRate = bundle.getFloat("key_newDollar",0.1f);
+            euroRate = bundle.getFloat("key_newEuro",0.1f);
+            wonRate = bundle.getFloat("key_newWon",0.1f);
+
+            Log.i(TAG, "onActivityResult: dollarRate=" + dollarRate);
+            Log.i(TAG, "onActivityResult: euroRate=" + euroRate);
+            Log.i(TAG, "onActivityResult: wonRate=" + wonRate);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.act_settings) {
+            openAdd();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
